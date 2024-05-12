d323fa3b349567db2ecae415c951875a0e3e6d0e
RuslanR <ruscool79@gmail.com>
2024-05-12 16:56:38 +0300

    add Task 11.1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        TaskNumber numberTask = new TaskNumber();
+
+        numberTask.printNumberTask("11.1.1");
+
+        AgeCalculator age = new AgeCalculator();
+//        age.ageDetermination();
+
+        numberTask.printNumberTask("11.1.2");
+
+        EventScheduler newEvent =new EventScheduler();
+        newEvent.findNextEvent();
+    }
+}
